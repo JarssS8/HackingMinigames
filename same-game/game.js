fcd0ef95be33ae30fef390f25ad63dc0b4248641
JarssS8 <47742360+JarssS8@users.noreply.github.com>
2024-02-07 18:42:20 +0000

    Remove stuff

--- same-game/game.js
+++ same-game/game.js
@@ -1,28 +1,3 @@
-//      game.js - same game HTML5 canvas implementation
-//      Copyright (C) 2010 Renaudeau Gaetan <contact@grenlibre.fr>
-//      
-//      This program is free software; you can redistribute it and/or modify
-//      it under the terms of the GNU General Public License as published by
-//      the Free Software Foundation; either version 2 of the License, or
-//      (at your option) any later version.
-//      
-//      This program is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU General Public License for more details.
-//      
-//      You should have received a copy of the GNU General Public License
-//      along with this program; if not, write to the Free Software
-//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//      MA 02110-1301, USA.
-
-/**
- * ChangeLog
- * 06 march 2010 - error managment + improve layout
- * 24 fev 2010 - Bugfixes
- * 21 fev 2010 - Initial release
- */
-
 (function(){
   var game = window.game = {};
   
